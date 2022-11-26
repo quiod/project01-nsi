e2cbb0485a4af615e1e0502173bdff846dce2913
quiod <115221083+quiod@users.noreply.github.com>
2022-11-26 01:04:38 +0100

    Update programme+bouton_negatif_etienne_v1.py

--- programme+bouton_negatif_etienne_v1.py
+++ programme+bouton_negatif_etienne_v1.py
@@ -1,9 +1,15 @@
-﻿from tkinter import *
+from tkinter import *
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfilename
 import PIL.ImageOps
 
 
+def interchangement():
+    stock=10
+    setimg(stock)
+    
+
+#fonction qui met la deuxieme image en negatif
 def negatif():
     stock =PIL.ImageOps.invert(resize_image)
     stock2 = ImageTk.PhotoImage(stock)
@@ -12,32 +18,37 @@ def negatif():
     labelS.place(x=580,y=0)
 
 
+def setimg(image):
+    #importation+mise en place première image
+     #reajustement de l'image par rapport au support
+    img = ImageTk.PhotoImage(resize_image)
+    label1 = Label(image=img)
+    label1.image = img
+    
+    #image 2 initialisation
+    label2 = Label(image=img)
+    label2.image = img
+    
+    label1.place(x=0,y=0)
+    label2.place(x=580,y=0)
+
+
 #creation fenètre
 root = Tk()
 root.title("editeur photo")
 root.geometry("1080x720")
 root.config(background='grey')
 
-#importation+mise en place première image
-image = Image.open(askopenfilename())
-resize_image = image.resize((500,int((500/image.size[0])*image.size[1]))) #reajustement de l'image par rapport au support
-img = ImageTk.PhotoImage(resize_image)
-label1 = Label(image=img)
-label1.image = img
+image=Image.open(askopenfilename())
+rezise_image=resize_image = image.resize((500,int((500/image.size[0])*image.size[1])))
+setimg(resize_image)
 
-#image 2 initialisation
-label2 = Label(image=img)
-label2.image = img
-
-label1.place(x=0,y=0)
-label2.place(x=520,y=0)
-
-
-#fonction qui met la deuxieme image en negatif
 
 #bouton du negatif
 btn = Button(root, text ="Cliquez ici!", command = negatif)
-btn.pack()
+btn.pack(side=TOP)
 
+btn1 = Button(root, text ="Cliquez ici!", command = interchangement)
+btn1.pack(side=BOTTOM)
 
 root.mainloop()
